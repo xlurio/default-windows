38829a8ba8c506b22e9254f4770590fb4c58d7ee
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-05-03 13:18:24 -0300

    Window with two combo boxes added

--- src/main/java/com/calegario/defaultwins/BoxCombo.java
+++ src/main/java/com/calegario/defaultwins/BoxCombo.java
@@ -4,12 +4,18 @@ import java.awt.*;
 import javax.swing.*;
 
 public class BoxCombo extends JPanel {
+    private JComboBox combo;
+
     public BoxCombo(String[] listOfOptions){
-        JComboBox combo = new JComboBox(listOfOptions);
+        combo = new JComboBox(listOfOptions);
 
         setLayout(new FlowLayout());
         setAlignmentX(Component.CENTER_ALIGNMENT);
         setBorder(new ComponentsGap());
         add(combo);
+    }
+
+    public JComboBox getCombo(){
+        return combo;
     }
 }
--- src/main/java/com/calegario/defaultwins/twocombobox/TwoComboBox.java
+++ src/main/java/com/calegario/defaultwins/twocombobox/TwoComboBox.java
@@ -6,8 +6,10 @@ import javax.swing.*;
 import com.calegario.defaultwins.*;
 
 public class TwoComboBox extends JFrame {
-    private BoxBtn button;
     private JPanel mainPanel;
+    private BoxCombo combo1;
+    private BoxCombo combo2;
+    private BoxBtn button;
 
     public TwoComboBox() {
         this(
@@ -26,6 +28,8 @@ public class TwoComboBox extends JFrame {
     public TwoComboBox(String title, String msg, String[] listOfOptions1,
                        String[] listOfOptions2, int width, int height) {
         super(title);
+        combo1 = new BoxCombo(listOfOptions1);
+        combo2 = new BoxCombo(listOfOptions2);
         button = new BoxBtn("OK", new DefaultActionListener(this));
 
         mainPanel = new JPanel();
@@ -33,8 +37,8 @@ public class TwoComboBox extends JFrame {
         mainPanel.setSize(width, height);
         mainPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         mainPanel.add(new BoxMsg(msg));
-        mainPanel.add(new BoxCombo(listOfOptions1));
-        mainPanel.add(new BoxCombo(listOfOptions2));
+        mainPanel.add(combo1);
+        mainPanel.add(combo2);
         mainPanel.add(button);
 
         add(mainPanel);
@@ -44,13 +48,21 @@ public class TwoComboBox extends JFrame {
     }
 
     public void setBtnListener(ActionListener listener){
-      mainPanel.remove(button);
-      String btnAction = button.getButton().getText();
-      button = new BoxBtn(btnAction, listener);
-      mainPanel.add(button);
+        mainPanel.remove(button);
+        String btnAction = button.getButton().getText();
+        button = new BoxBtn(btnAction, listener);
+        mainPanel.add(button);
+    }
+
+    public BoxCombo getCombo1() {
+        return combo1;
+    }
+
+    public BoxCombo getCombo2() {
+        return combo2;
     }
 
     public void showBox(){
-      setVisible(true);
+        setVisible(true);
     }
 }
